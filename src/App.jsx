abf862973020d8ad51ffbf6fcbc4d9f24e9449b4
Kelvin Chansa <chansakelvin438@gmail.com>
2026-05-25 19:54:38 +0300

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
@@ -27,6 +28,7 @@ function App() {
       <WhatsAppButton />
 
       <Footer />
+      <Analytics />
     </main>
   )
 }
