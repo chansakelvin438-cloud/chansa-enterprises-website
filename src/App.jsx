23678142e97bf2755f7dfe28498c5a532f64beeb
Kelvin Chansa <chansakelvin438@gmail.com>
2026-05-25 20:12:38 +0300

    Add Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
@@ -29,6 +30,7 @@ function App() {
 
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </main>
   )
 }
